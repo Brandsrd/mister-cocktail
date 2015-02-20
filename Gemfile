192fc9d1e0effc85058c4b7059b597c6098c9bdd
Ruben Brands <rdbrands@gmail.com>
2015-02-20 14:36:54 +0100

    image upload working

--- Gemfile
+++ Gemfile
@@ -1,6 +1,9 @@
 ruby '2.2.0'
 source 'https://rubygems.org'
 
+gem 'aws-sdk', '< 2.0'
+gem 'paperclip'
+
 gem 'figaro'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.0'
--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -5,6 +5,13 @@ class Cocktail < ActiveRecord::Base
 
 
   validates :name, presence: true, uniqueness: true
+
+    has_attached_file :picture,
+    styles: { medium: "300x300>", thumb: "100x100>" }
+
+  validates_attachment_content_type :picture,
+    content_type: /\Aimage\/.*\z/
+
 end
 
 
